57ebf35cdbede9d77fbf423157785002fed9b30d
keeks5456 <Akikogreen08@gmail.com>
2020-08-18 15:08:24 -0700

    stuff

--- anime-frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-  // const baseURL = "https://api.jikan.moe/v3/anime/1/characters/1" //main api data
-
-  // fetch(baseURL)
-  // .then(res => res.json())
-  // .then(json => console.log(json))
-  // .catch(error => console.log(error.message))
