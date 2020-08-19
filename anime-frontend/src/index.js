7981d5b17725a8dc94847db2358f81d5cedc6970
keeks5456 <Akikogreen08@gmail.com>
2020-08-19 15:46:59 -0700

    commits

--- anime-frontend/src/index.js
+++ anime-frontend/src/index.js
@@ -5,32 +5,34 @@ const userContainer = document.querySelector('.container')
   const fetchUsers = () => {
     fetch(baseURL)
     .then(res => res.json())
-    .then(json => json.forEach(user => buildUserCard(user))) //array
+    .then(json => json.forEach(post => buildPostCard(post))) //array
   }
   fetchUsers()
 
   // Build the user's cards
 
-  const buildUserCard = (user) =>{
-    console.log(user)
-    const userCard = document.createElement('div')
-    userCard.className = "card"
-    userCard.id = user.id
-    userCard.innerHTML += `
+  const buildPostCard = (post) =>{
+    console.log(post)
+    const postCard = document.createElement('div')
+    postCard.className = "card"
+    postCard.id = post.id
+    postCard.innerHTML += `
      
-        <h1>${user.username}</h1>
-        <img src="${user.artwork}"> 
-        <p>${user.description}</p>
+        <h1>${post.username}</h1> //this will have user, needs serializer first
+        <img src="${post.artwork}"> 
+        <p>${post.description}</p>
         <form action="/html/tags/html_form_tag_action.cfm" method="post">
         <div>
         <textarea name="comments" id="comments" style="font-family:sans-serif;font-size:1.2em;">Hey... say something!</textarea>
         </div>
         <input type="submit" value="Submit">
         </form> 
-        <button class='btn-success'>likes: 0</button>
+        <button class='btn-success'>likes: ${post.likes}</button>
         <button class='btn-danger' data-delete='delete-btn' >Delete</button>
         <button class='btn-primary' data-edit='edit-btn'>Edit</button> 
           `
+
+
     userContainer.appendChild(userCard)
     console.log(userCard)
   }
